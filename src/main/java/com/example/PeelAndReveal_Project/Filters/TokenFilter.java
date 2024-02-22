182f2bdf4685bd872fa0f37f8364d74a5cc22657
Tomer2H <tomer2b0525@gmail.com>
2024-02-22 23:10:42 +0200

    updated token filter, to check for expired tokens and remove from sessionMap

--- src/main/java/com/example/PeelAndReveal_Project/Filters/TokenFilter.java
+++ src/main/java/com/example/PeelAndReveal_Project/Filters/TokenFilter.java
@@ -2,6 +2,10 @@ package com.example.PeelAndReveal_Project.Filters;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.example.PeelAndReveal_Project.Controllers.LoginController;
+import com.example.PeelAndReveal_Project.Exceptions.LoginFailedException;
+import com.example.PeelAndReveal_Project.Services.ClientService;
+import com.example.PeelAndReveal_Project.Services.LoginManager;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,11 +14,23 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 @Component
 @Order(2)
 public class TokenFilter extends OncePerRequestFilter {
+
+    private final LoginController logincontroller;
+    private final HashMap<String, ClientService> sessionStore;
+
+    public TokenFilter(LoginController logincontroller, HashMap<String, ClientService> sessionStore) {
+        this.logincontroller = logincontroller;
+        this.sessionStore = sessionStore;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = request.getHeader("Authorization");
@@ -25,14 +41,33 @@ public class TokenFilter extends OncePerRequestFilter {
         }else {
             //we got a token
             token = token.replace("Bearer ","");
+
             try {
+
                 JWT.decode(token).getClaim("role").asString();
-                //if there is role in the token it won't throw exception, and you can move to the next step
-                filterChain.doFilter(request,response);
+                if (sessionStore.containsKey(token)) {
+                Date exp = JWT.decode(token).getExpiresAt();
+
+                    if (exp.after(new Date())) {
+                        //if there is role in the token it won't throw exception, and you can move to the next step
+                        filterChain.doFilter(request, response);
+                    }else {
+                        System.out.println(LocalDateTime.now()+" : deleted expired token from map :  " +token);
+                        sessionStore.remove(token);
+                        throw new LoginFailedException("Login token expired please log in again");
+                    }
+                }else {
+                    System.out.println(LocalDateTime.now() +" : No token found : "+token+"\nsent status code 403! ^^ ");
+                    response.setStatus(403);
+                    response.getWriter().write("You must log in!");
+                }
                 //if there is not (probably someone constructed a token and tries to mess with us)
             }catch (JWTDecodeException ex){
                 response.setStatus(403);
                 response.getWriter().write("You must log in!");
+            } catch (LoginFailedException e) {
+                response.setStatus(403);
+                response.getWriter().write(e.getMessage());
             }
         }
     }
