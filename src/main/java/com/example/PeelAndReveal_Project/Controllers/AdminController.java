5a098840b951c2104943ec3da1f8441cbed50cbf
Tomer2H <tomer2b0525@gmail.com>
2024-02-22 23:04:52 +0200

    Added the usage of token to adminController, and updated delete functions to customer and company to remove token from sessionMap

--- src/main/java/com/example/PeelAndReveal_Project/Controllers/AdminController.java
+++ src/main/java/com/example/PeelAndReveal_Project/Controllers/AdminController.java
@@ -2,112 +2,159 @@ package com.example.PeelAndReveal_Project.Controllers;
 
 import com.example.PeelAndReveal_Project.EntityBeans.Company;
 import com.example.PeelAndReveal_Project.EntityBeans.Customer;
-import com.example.PeelAndReveal_Project.Exceptions.EmailExistsException;
-import com.example.PeelAndReveal_Project.Exceptions.IdNotFoundException;
-import com.example.PeelAndReveal_Project.Exceptions.IdNotZeroException;
-import com.example.PeelAndReveal_Project.Exceptions.NameExistsException;
+import com.example.PeelAndReveal_Project.Exceptions.*;
 import com.example.PeelAndReveal_Project.Services.AdminService;
+import com.example.PeelAndReveal_Project.Services.ClientService;
+import com.example.PeelAndReveal_Project.Services.CompanyService;
+import com.example.PeelAndReveal_Project.Services.CustomerService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class AdminController {
-    @Autowired
-    AdminService admin;
+    private final HttpServletRequest request;
+    private final HashMap<String, ClientService> sessionStore;
 
-    @GetMapping("company")
-    public ResponseEntity<Company> getOneCompany(int companyID) throws IdNotFoundException {
+    public AdminController(HttpServletRequest request, HashMap<String, ClientService> sessionStore) {
+        this.request = request;
+        this.sessionStore = sessionStore;
+    }
+
+    @GetMapping("company/{companyId}")
+    public ResponseEntity<Company> getOneCompany(@PathVariable int companyId) throws IdNotFoundException, LoginFailedException {
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(admin.getOneCompanyByID(companyID));
+                .body(getService().getOneCompanyByID(companyId));
     }
 
     @GetMapping("companies")
-    public ResponseEntity<List<Company>> getAllCompanies() {
+    public ResponseEntity<List<Company>> getAllCompanies() throws LoginFailedException {
+        System.out.println(LocalDateTime.now() + "   AdminController : Sent All companies from database!");
         return ResponseEntity
                 .status(HttpStatus.ACCEPTED)
-                .body(admin.getAllCompanies());
+                .body(getService().getAllCompanies());
     }
 
-    @PutMapping("comapny")
-    public ResponseEntity<String> updateCompany(@RequestBody Company company) throws IdNotFoundException, NameExistsException {
-        admin.updateCompany(company);
+    @PutMapping("company")
+    public ResponseEntity<String> updateCompany(@RequestBody Company company) throws IdNotFoundException, NameExistsException, LoginFailedException, EmailExistsException {
+        getService().updateCompany(company);
+        System.out.println(LocalDateTime.now() + "   AdminController : Updated company with  id = "+company.getId());
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body("Successfully updated!");
     }
 
     @PostMapping("company")
-    public ResponseEntity<Company> addCompany(@RequestBody Company company) throws EmailExistsException, NameExistsException {
-        Company newCompany = admin.addCompany(company);
+    public ResponseEntity<Company> addCompany(@RequestBody Company company) throws EmailExistsException, NameExistsException, LoginFailedException {
+        Company newCompany = getService().addCompany(company);
+        System.out.println(LocalDateTime.now() + "   AdminController : Added new company! , company id = "+newCompany.getId());
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(newCompany);
     }
 
-    @DeleteMapping("company")
-    public ResponseEntity<String> deleteCompany(int companyID) throws IdNotFoundException {
-        if (admin.deleteCompany(companyID)) {
+    @DeleteMapping("company/{companyId}")
+    public ResponseEntity<String> deleteCompany(@PathVariable  int companyId) throws IdNotFoundException, LoginFailedException {
+        if (getService().deleteCompany(companyId)) {
+            //used iterator instead of map.foreach() because of ConcurrentModificationException
+            // that occurs when I try to delete a key (with the object) from the map while in foreach
+            Iterator<Map.Entry<String, ClientService>> iterator = sessionStore.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Map.Entry<String, ClientService> entry = iterator.next();
+                if (entry.getValue() instanceof CompanyService companyService) {
+                    if (companyService.getCompanyID() == companyId) {
+                        iterator.remove();
+                        System.out.println(LocalDateTime.now() + "   AdminController : while removing company with id "+companyId+" found session token, deleting session token!");
+                    }
+                }
+            }
+            System.out.println(LocalDateTime.now() + "   AdminController : Deleted company with id = "+companyId);
             return ResponseEntity
                     .status(HttpStatus.ACCEPTED)
                     .body("Deleted Successfully!");
 
         } else return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body("Company with the id " + companyID + " was not found (i guess its good?)");
+                .body("Company with the id " + companyId + " was not found (i guess its good?)");
     }
 
     @GetMapping("customers")
-    public ResponseEntity<List<Customer>> getAllCustomers() {
+    public ResponseEntity<List<Customer>> getAllCustomers() throws LoginFailedException {
+        System.out.println(LocalDateTime.now() + "   AdminController : Sent All customers from database!");
         return ResponseEntity
                 .status(HttpStatus.ACCEPTED)
-                .body(admin.getAllCustomers());
+                .body(getService().getAllCustomers());
     }
 
     @PostMapping("customer")
-    public ResponseEntity<Customer> addCustomer(@RequestBody Customer customer) throws EmailExistsException, IdNotZeroException {
-        Customer newCustomer = admin.addCustomer(customer);
+    public ResponseEntity<Customer> addCustomer(@RequestBody Customer customer) throws EmailExistsException, IdNotZeroException, LoginFailedException {
+        Customer newCustomer = getService().addCustomer(customer);
+        System.out.println(LocalDateTime.now() + "   AdminController : Added new customer! , new customer id = "+newCustomer.getCustomerID());
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(newCustomer);
     }
 
-    @GetMapping("customer")
-    public ResponseEntity<Customer> getOneCustomer(int customerID) throws IdNotFoundException {
+    @GetMapping("customer/{customerID}")
+    public ResponseEntity<Customer> getOneCustomer(@PathVariable int customerID) throws IdNotFoundException, LoginFailedException {
         return ResponseEntity
-                .status(HttpStatus.FOUND)
-                .body(admin.getOneCustomerByID(customerID));
+                .status(HttpStatus.OK)
+                .body(getService().getOneCustomerByID(customerID));
     }
 
 
     @PutMapping("customer")
-    public ResponseEntity<String> updateCustomer(@RequestBody Customer customer) throws EmailExistsException, IdNotFoundException {
-        admin.updateCustomer(customer);
+    public ResponseEntity<String> updateCustomer(@RequestBody Customer customer) throws EmailExistsException, IdNotFoundException, LoginFailedException {
+        getService().updateCustomer(customer);
+        System.out.println(LocalDateTime.now() + "   AdminController : Updated customer with id = "+customer.getCustomerID());
         return ResponseEntity
                 .status(HttpStatus.ACCEPTED)
                 .body("Updated successfully!");
     }
 
-    @DeleteMapping("customer")
-    public ResponseEntity<String> deleteCustomer(int customerID) throws IdNotFoundException {
-        if (admin.deleteCustomer(customerID)) {
+    @DeleteMapping("customer/{customerId}")
+    public ResponseEntity<String> deleteCustomer(@PathVariable int customerId) throws IdNotFoundException, LoginFailedException {
+        if (getService().deleteCustomer(customerId)) {
+            //used iterator instead of map.foreach() because of ConcurrentModificationException
+            // that occurs when I try to delete a key (with the object) from the map while in foreach
+            Iterator<Map.Entry<String, ClientService>> iterator = sessionStore.entrySet().iterator();
+            while (iterator.hasNext()) {
+                Map.Entry<String, ClientService> entry = iterator.next();
+                if (entry.getValue() instanceof CustomerService customerService) {
+                    if (customerService.getCustomerID() == customerId) {
+                        iterator.remove();
+                        System.out.println(LocalDateTime.now() + "   AdminController : while deleting customer with id "+customerId+" found session token, deleting session token!");
+                    }
+                }
+            }
+            System.out.println(LocalDateTime.now() + "   AdminController : Deleted customer with id = "+customerId);
             return ResponseEntity
                     .status(HttpStatus.ACCEPTED)
                     .body("Deleted Successfully!");
 
         } else return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body("Company with the id " + customerID + " was not found (i guess its good?)");
+                .body("Company with the id " + customerId + " was not found (i guess its good?)");
     }
 
-//    private CompanyService getService(){
-//        String token = request.getHeader
-//
-//    }
+    private AdminService getService() throws LoginFailedException {
+        String token = request.getHeader("Authorization");
+        AdminService admin = (AdminService) sessionStore.get(token);
+        if (admin == null) {
+            System.out.println(LocalDateTime.now() + "   AdminController : Failed to get AdminService!");
+            throw new LoginFailedException("Login information expired");
+        }else return admin;
+
+    }
 }
 
 
